5d445590e75cf7c4d211c15a57a82f28d823332e
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-05-16 10:54:38 -0300

    🚏 feat: add react-router-dom and configure app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./styles/Themes/default"
-import { GlobalStyle } from "./styles/global"
-
-
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/Themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Components/Router";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Ola</h1>
-
-      <GlobalStyle/>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
