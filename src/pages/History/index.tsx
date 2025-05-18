3090d1721ee32fcd053819310ba4bf1976370c72
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-05-18 13:03:48 -0300

    Status component style

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./style";
+import { HistoryContainer, HistoryList, Status } from "./style";
 
 export function History() {
   return (
@@ -16,28 +16,36 @@ export function History() {
           </thead>
           <tbody>
             <tr>
-              <td>Estudar JavaScript</td>
+              <td>Study JavaScript</td>
               <td>25min</td>
-              <td>Há 1 semana</td>
-              <td>Concluído</td>
+              <td>1 week ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
             </tr>
             <tr>
-              <td>Ler artigo sobre React</td>
+              <td>Read React article</td>
               <td>15min</td>
-              <td>Há 3 dias</td>
-              <td>Concluído</td>
+              <td>3 days ago</td>
+              <td>
+                <Status statusColor="yellow">In progress</Status>
+              </td>
             </tr>
             <tr>
-              <td>Revisar código</td>
+              <td>Review code</td>
               <td>40min</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="red">Pending</Status>
+              </td>
             </tr>
             <tr>
-              <td>Planejar nova feature</td>
+              <td>Plan new feature</td>
               <td>30min</td>
-              <td>Ontem</td>
-              <td>Concluído</td>
+              <td>Yesterday</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
             </tr>
           </tbody>
         </table>
